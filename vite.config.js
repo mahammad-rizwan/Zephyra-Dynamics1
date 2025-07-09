9380ff46ea2d45f01ecd969f76e072a679af91d0
mahammad-rizwan <mahammadrizwan704@gmail.com>
2025-07-09 21:23:44 +0530

    mm

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Zephyra-Dynamics/',
+  base: '/Zephyra-Dynamics1/',
 })
